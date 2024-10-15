bb892707e63f90b6c068555b2d56a966d37174c7
Flattinger.Fabian <Flattinger.Fabian@fronius.com>
2024-10-15 11:17:51 +0200

    Add Pages for management

---
+++ Shoop24.Administration/Components/Pages/Management/UserManagement.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shoop24.Administration.Components.Pages.Management
+{
+    public partial class UserManagement : ComponentBase
+    {
+
+    }
+}
