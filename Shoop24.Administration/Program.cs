a4ffb924450c44b6154dd3610ffe14d2aca83c4c
Flattinger.Fabian <Flattinger.Fabian@fronius.com>
2024-10-15 11:13:39 +0200

    Add project files.

---
+++ Shoop24.Administration/Program.cs
@@ -0,0 +1,43 @@
+using MudBlazor;
+using MudBlazor.Services;
+using Shoop24.Administration.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+builder.Services.AddMudServices();
+builder.Services.AddMudBlazorDialog();
+builder.Services.AddSignalR();
+builder.Services.AddMudBlazorSnackbar(o =>
+{
+    o.NewestOnTop = true;
+    o.HideTransitionDuration = 200;
+    o.ShowTransitionDuration = 300;
+    o.SnackbarVariant = Variant.Filled;
+    o.MaxDisplayedSnackbars = 5;
+    o.PositionClass = Defaults.Classes.Position.TopCenter;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ Shoop24.Administration/wwwroot/js/index.js
@@ -0,0 +1,20 @@
+﻿function GetDarkMode(){
+    return localStorage.getItem("isDarkMode") === "true";
+}
+
+function SetDarkMode(isDarkMode){
+    ToggleDarkMode();
+    
+    localStorage.setItem("isDarkMode", isDarkMode);
+}
+
+function ToggleDarkMode() {
+    document.body.classList.toggle("fro-dark-context");
+    document.body.classList.toggle("fro-dark-background");
+
+    var html = document.getElementsByTagName("html")[0];
+    html.classList.toggle("fro-dark-context");
+
+    var isDarkMode = document.body.classList.contains("fro-dark-context");
+    window.localStorage.setItem("isDarkMode", isDarkMode);
+}
