217c5f03a70d627a2f2e58792d3b603c2e5e7217
Fabian Flattinger <fabiflatti@gmail.com>
2024-10-21 00:27:38 +0200

    Add Delete Function to Product and AskDialog.razor

--- Shoop24.Administration/Components/Pages/Management/ProductManagement.razor.cs
+++ Shoop24.Administration/Components/Pages/Management/ProductManagement.razor.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.VisualBasic.CompilerServices;
 using MudBlazor;
 using Shoop24.Administration.Components.Dialogs;
 using Shoop24.Core.Enums;
@@ -23,7 +24,29 @@ namespace Shoop24.Administration.Components.Pages.Management
         {
             
         }
-        
+
+        private string[] searchValues = new[] { "Bier", "Fleisch", "Wurst", "Nudeln" };
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                Random r = new Random();
+                var searchValue = searchValues[r.Next(searchValues.Count())];
+                _isLoading = true;
+                await InvokeAsync(StateHasChanged);
+                var result = await _client.GetProductResolver().GetProductsBySearchValueAsyncAsync(searchValue);
+                if (result.ErrorCode != ShoopErrorCode.NoError)
+                {
+                    _snackbar.Add($"Fehler beim laden der Produkte ({result.ErrorCode})", Severity.Error);
+                    return;
+                }
+
+                _isLoading = false;
+                _foundProducts = result.Result;
+                await InvokeAsync(StateHasChanged);
+            }
+        }
+
         public async Task ExecuteSearchCommand()
         {
             _isLoading = true;
@@ -54,30 +77,46 @@ namespace Shoop24.Administration.Components.Pages.Management
             };
             var dialogReference = await _dialogService.ShowAsync<EditProductDialog>("", dialogParameters);
             var result = await dialogReference.Result;
+            if (result != null && !result.Canceled)
+            {
+                var data = (Product)result.Data!;
+                var changeResult = await _client.GetProductResolver().UpdateProductDetailsAsync(product.Id, data);
+                if (changeResult != null && changeResult.ErrorCode != ShoopErrorCode.NoError)
+                {
+                    _snackbar.Add("Produkt konnte nicht aktualisiert werden", Severity.Error);
+                    return;
+                }
+
+                _snackbar.Add("Produkt wurde erfolgreich aktualisiert", Severity.Success);
+                
+            }
+        }
+
+        private async Task DeleteProduct(Product product)
+        {
+            var settings = new DialogOptions()
+            {
+                FullWidth = true,
+                MaxWidth = MaxWidth.Large
+            };
+            var dialogParameters = new DialogParameters()
+            {
+                ["Message"] = $"Möchtes du das Product {product.Name} wirklich löschen?",
+            };
+            var dialogReference = await _dialogService.ShowAsync<AskDialog>("", dialogParameters);
+            var result = await dialogReference.Result;
             if (!result.Canceled)
             {
-                try
+                var deleteResult = await _client.GetProductResolver().DeleteProductByIdAsync(product.Id);
+                if (deleteResult.ErrorCode != ShoopErrorCode.NoError)
                 {
-                    var data = (Product)result.Data;
-                    if (data == null)
-                    {
-                        _snackbar.Add("Daten konnten nicht geändert werden", Severity.Error);
-                        return;
-                    }
-
-                    var changeResult = await _client.GetProductResolver().UpdateProductDetailsAsync(product.Id, data);
-                    if (changeResult.ErrorCode != ShoopErrorCode.NoError)
-                    {
-                        _snackbar.Add("Produkt konnte nicht aktualisiert werden", Severity.Error);
-                        return;
-                    }
-
-                    _snackbar.Add("Produkt wurde erfolgreich aktualisiert", Severity.Success);
+                    _snackbar.Add($"Produkt konnte nicht gelöscht werden ({deleteResult.ErrorCode})", Severity.Error);
+                    return;
                 }
-                catch (Exception e)
-                {
-                    
-                }
+
+                _snackbar.Add($"Produkt {product.Name} wurde erfolgreich gelöscht", Severity.Success);
+                _foundProducts.Remove(product);
+                await InvokeAsync(StateHasChanged);
             }
         }
     }
